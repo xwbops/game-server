54d3115cbe79d28e027f282a97dcdeef61a92fdf
超级管理员1 <admin@example.com>
2024-01-09 18:32:20 +0800

    fix

--- src/zinx/znet/connection.go
+++ src/zinx/znet/connection.go
@@ -13,8 +13,6 @@ type Connection struct {
 	ConnID uint32
 	//当前连接的关闭状态
 	isClosed bool
-	// 获取远程地址
-	RemoteAddr *net.TCPAddr
 	//该连接的处理方法api
 	handleAPI ziface.HandFunc
 
@@ -36,6 +34,32 @@ func NewConnection(conn *net.TCPConn, connID uint32, callback_api ziface.HandFun
 /*处理conn读数据的Goroutine*/
 func (c *Connection) StartReader() {
 	fmt.Println("Reader Goroutine is running")
-	defer fmt.Println(c.RemoteAddr.String(), " conn reader exit!")
+	defer fmt.Println(c.RemoteAddr().String(), " conn reader exit!")
 	defer c.Stop()
+	
+}
+
+//停止连接，结束当前连接状态M
+func (c *Connection) Stop() {
+	//1. 如果当前链接已经关闭
+	if c.isClosed == true {
+		return
+	}
+	c.isClosed = true
+
+	//TODO Connection Stop() 如果用户注册了该链接的关闭回调业务，那么在此刻应该显示调用
+
+	// 关闭socket链接
+	c.Conn.Close()
+
+	//通知从缓冲队列读数据的业务，该链接已经关闭
+	c.ExitBuffChan <- true
+
+	//关闭该链接全部管道
+	close(c.ExitBuffChan)
+}
+
+//获取远程客户端地址信息
+func (c *Connection) RemoteAddr() net.Addr {
+	return c.Conn.RemoteAddr()
 }
