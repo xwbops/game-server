990fb4a27ec1463e5a93d03904a439cc6ec2c1e7
超级管理员1 <admin@example.com>
2024-01-09 16:37:28 +0800

    fix

---
+++ src/zinx/ziface/iconnection.go
@@ -0,0 +1,18 @@
+package ziface
+
+import "net"
+
+//定义连接接口
+type IConnection interface {
+	//启动连接，让当前连接开始工作
+	Start()
+	//停止连接，结束当前连接状态
+	Stop()
+	//从当前连接获取原始的socket TCPConn GetTCPConnection *net.TCPConn //获取当前连接ID
+	GetConnID() uint32 //获取远程客户端地址信息 RemoteAddr() net.Addr
+	// 获取远程客户端地址
+	RemoteAddr() *net.TCPAddr
+}
+
+//定义⼀一个统⼀一处理理链接业务的接⼝口
+type HandFunc func(*net.TCPConn, []byte, int) error
---
+++ src/zinx/znet/connection.go
@@ -0,0 +1,40 @@
+package znet
+
+import (
+	"fmt"
+	"net"
+	"zinx/ziface"
+)
+
+type Connection struct {
+	//当前连接的socket TCP套接字
+	Conn *net.TCPConn
+	//当前连接的ID 也可以称作为SessionID，ID全局唯一
+	ConnID uint32
+	//当前连接的关闭状态
+	isClosed bool
+	// 获取远程地址
+	RemoteAddr *net.TCPAddr
+	//该连接的处理方法api
+	handleAPI ziface.HandFunc
+
+	//告知该链接已经退出/停止的channel
+	ExitBuffChan chan bool
+}
+
+func NewConnection(conn *net.TCPConn, connID uint32, callback_api ziface.HandFunc) *Connection {
+	c := &Connection{
+		Conn:         conn,
+		ConnID:       connID,
+		isClosed:     false,
+		handleAPI:    callback_api,
+		ExitBuffChan: make(chan bool, 1),
+	}
+	return c
+}
+
+/*处理conn读数据的Goroutine*/
+func (c *Connection) StartReader() {
+	fmt.Println("Reader Goroutine is running")
+	defer fmt.Println(c.RemoteAddr.String())
+}
