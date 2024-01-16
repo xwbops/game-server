cb568ebdab9bbc76a67f81765242a26619948f16
bin wen <binwen@bindeMac-mini.local>
2024-01-16 22:44:47 +0800

    base done

--- src/zinx/example/v5/Server.go
+++ src/zinx/example/v5/Server.go
@@ -15,7 +15,7 @@ type PingRouter struct {
 func (this *PingRouter) Handle(request ziface.IRequest) {
 	fmt.Println("Call PingRouter Handle")
 	//先读取客户端的数据，再回写ping...ping...ping
-	fmt.Println("recv from client : msgId=", request.GetMsgId(), ", data=", string(request.GetData()))
+	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
 
 	err := request.GetConnection().SendBuffMsg(0, []byte("ping...ping...ping"))
 	if err != nil {
@@ -31,7 +31,7 @@ type HelloZinxRouter struct {
 func (this *HelloZinxRouter) Handle(request ziface.IRequest) {
 	fmt.Println("Call HelloZinxRouter Handle")
 	//先读取客户端的数据，再回写ping...ping...ping
-	fmt.Println("recv from client : msgId=", request.GetMsgId(), ", data=", string(request.GetData()))
+	fmt.Println("recv from client : msgId=", request.GetMsgID(), ", data=", string(request.GetData()))
 
 	err := request.GetConnection().SendBuffMsg(1, []byte("Hello Zinx Router V0.8"))
 	if err != nil {
